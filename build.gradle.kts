c4b90b2bd4c5f7154919f5188994595e81011fc8
Yumin Kim <green4469@gmail.com>
2023-07-30 00:23:13 +0900

    feat: setup kotest dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,10 +30,11 @@ subprojects {
     apply {
         plugin("org.springframework.boot")
         plugin("io.spring.dependency-management")
-        plugin("org.jlleitschuh.gradle.ktlint")
-
         plugin("kotlin")
         plugin("kotlin-spring")
+
+        plugin("org.jlleitschuh.gradle.ktlint")
+        plugin("org.gradle.java-test-fixtures")
     }
 
     repositories {
@@ -43,8 +44,15 @@ subprojects {
     dependencies {
         implementation("org.springframework.boot:spring-boot-starter")
         implementation("org.jetbrains.kotlin:kotlin-reflect")
+
         annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
         testImplementation("org.springframework.boot:spring-boot-starter-test")
+        testImplementation("io.kotest:kotest-runner-junit5:5.6.2")
+        testImplementation("io.kotest:kotest-assertions-core:5.6.2")
+        testImplementation("io.kotest:kotest-framework-dataset:5.6.2")
+        testImplementation("io.kotest:kotest-extensions-spring:4.4.3")
+        testImplementation("io.mockk:mockk:1.13.5")
     }
 
     tasks.withType<KotlinCompile> {
