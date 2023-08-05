adf08e3009920d8268493ca7bb267f765ab557b6
Yumin Kim <green4469@gmail.com>
2023-08-05 23:46:58 +0900

    feat: implement pipeline test result

--- plumber-core/src/main/kotlin/com/yuminkim/plumber/core/model/tester/PipelineTestResult.kt
+++ plumber-core/src/main/kotlin/com/yuminkim/plumber/core/model/tester/PipelineTestResult.kt
@@ -1,5 +1,17 @@
 package com.yuminkim.plumber.core.model.tester
 
 class PipelineTestResult(
+  val status: PipelineTestStatus,
+  val reason: String,
+  val detail: PipelineTestResultDetail,
+)
+
+class PipelineTestResultDetail(
+  val stages: List<StageTestResult>,
   val status: PipelineTestStatus
+)
+
+class StageTestResult(
+  val name: String,
+  val status: StageTestStatus,
 )
